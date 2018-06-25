1c02d3219723e0313a83c8357af1ad31cb20c3f1
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-06-25 16:12:32 +0200

    a

---
+++ src/com/kole/Album.java
@@ -0,0 +1,21 @@
+package com.kole;
+
+import java.util.ArrayList;
+
+public class Album {
+    private String name;
+    private ArrayList<Song> songs;
+
+    public Album(String name) {
+        this.name = name;
+        this.songs = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+}
